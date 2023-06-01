77342e29ed83eb35ef5853287166c3075c1f7fee
PraiseImmanuel <praiseimmanuel601@gmail.com>
2023-06-01 08:53:59 +0100

    cleanup code

--- src/components/PricingTables.tsx
+++ src/components/PricingTables.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const PricingTables: React.FC<Props> = ({ children }) => {
     return (
-        <div className="pricing_tables flex space-x-6 md:space-x-3 mt-8 xl:mt-0 overflow-x-scroll xl:overflow-hidden md:mx-10">
+        <div className="pricing_tables space-x-6 md:space-x-3 mt-8 xl:mt-0 overflow-x-scroll xl:overflow-hidden md:mx-10">
             {children}
         </div>
     );
